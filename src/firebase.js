76130b77c2839fb0786721dcfed16ff46a48460c
Airus Apple <airus-macbook@Airusui-MacBookPro.local>
2022-07-12 22:58:53 +0900

    2.0 Firebase setting

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCI2HKtQdsdC_7-vIEwEdwXzr_k5Mv58zI",
+  authDomain: "nwitter-fd327.firebaseapp.com",
+  projectId: "nwitter-fd327",
+  storageBucket: "nwitter-fd327.appspot.com",
+  messagingSenderId: "449197892761",
+  appId: "1:449197892761:web:35e39914da9577c9544408"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
